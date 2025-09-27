c53b848e6399a16816afe458ad3d3fb744fc4dfa
Tushar <tusharbhatt0135@gmail.com>
2025-09-27 14:31:45 +0530

    updated schema

--- frontend/src/inngest/functions.ts
+++ frontend/src/inngest/functions.ts
@@ -2,11 +2,24 @@ import { env } from "~/env";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
-  { id: "process-video" },
-  { event: "test/hello.world" },
+  {
+    id: "process-video",
+    concurrency: {
+      limit: 1,
+      key: "event.data.userid",
+    },
+  },
+  { event: "process-video-events" },
   async ({ event, step }) => {
     await step.run("call-modal-endpoint", async () => {
-      await fetch(env.PROCESS_VIDEO_ENDPOINT);
+      await fetch(env.PROCESS_VIDEO_ENDPOINT, {
+        method: "POST",
+        body: JSON.stringify({ s3_key: "test1/min6-5mins.mp4" }),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${env.PROCESS_VIDEO_ENDPOINT_AUTH}`,
+        },
+      });
     });
   },
 );
